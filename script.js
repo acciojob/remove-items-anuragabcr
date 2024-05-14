c40f8d508a8efa6d72af0647dd814b781ce9d52c
Anurag Kumar <47923173+anuragabcr@users.noreply.github.com>
2024-05-14 15:29:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let colors = document.getElementById("colorSelect")
+// alert(JSON.stringify(colors)) 
+
+let button = document.querySelector("input[type='button'][value='Select and Remove']")
+
+button.addEventListener('click', function () {
+	let colorIndex = colors.selectedIndex
+	// let color = colors.options[colorIndex]
+	// alert(color.text, color.value)
+	colors.options.remove(colorIndex)
+})
